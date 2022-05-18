97445124a20b231928cb9f23e5b83f5b8cc18c46
ikeemthedream <55724332+ikeemthedream@users.noreply.github.com>
2022-05-18 13:26:17 -0400

    removing jump from the base movements

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,31 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public float speed = 10.0f;
-
-    public Rigidbody body;
+    private Rigidbody _body;
+    private float _run;
+    private float _jump;
+    
     // Start is called before the first frame update
     void Start()
     {
-       body = GetComponent<Rigidbody>();
+       _body = GetComponent<Rigidbody>();
+       _run = Input.GetAxis("Horizontal");
+       //_jump = Input.GetAxis("Vertical");
     }
     private Vector3 _movement = Vector3.zero;
 
     // Update is called once per frame
     void Update()
     {        
-        _movement = new Vector3(Input.GetAxis("Horizontal"),  Input.GetAxis("Vertical"), 0);
+        _movement = new Vector3(Input.GetAxis("Horizontal"),  0, 0);
         
-
     }
     
     private void FixedUpdate()
     {          
-        body.MovePosition(body.position + _movement * speed * Time.deltaTime);
+        _body.MovePosition(_body.position + _movement * (speed * Time.deltaTime));
 
     }
+
+    
 }
