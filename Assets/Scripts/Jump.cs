a654f9d6d517c2d621bdd1be82ed40200bd57478
ikeemthedream <55724332+ikeemthedream@users.noreply.github.com>
2022-05-22 22:39:16 -0400

    Added raycast to detect ground before jumpint

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -8,22 +8,33 @@ using Vector3 = UnityEngine.Vector3;
 public class Jump : MonoBehaviour
 {
     private Rigidbody rb;
-
-    // Start is called before the first frame update
+    public int maxDistance;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        int layerMask = 1 << 8;
+
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, maxDistance,
+                layerMask))
         {
-            rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            Debug.Log("Hit object");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * maxDistance, Color.blue);
+            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+                Debug.Log("Jump");
+            }
+        }
+        else
+        {
+            Debug.Log("Did not hit object");
         }
     }
-
-    
 }
