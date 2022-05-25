79d2dcd02df05871aa35c57dff257916b01431f6
ikeemthedream <55724332+ikeemthedream@users.noreply.github.com>
2022-05-25 15:43:44 -0400

    testing counter in jump script

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -9,7 +9,7 @@ public class Jump : MonoBehaviour
 {
     private Rigidbody rb;
     public float maxDistance = 2.0f;
-    public int counter = 2;
+    public int counter = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -35,17 +35,24 @@ public class Jump : MonoBehaviour
                 if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, maxDistance,
                         layerMask))
                 {
-                    Debug.Log("Hit object");
+                    //Debug.Log("Hit object");
                     
                     if (Input.GetKeyDown(KeyCode.Space))
                     {
                         rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
-                        Debug.Log("Jump");
+                        counter++;
+                        Debug.Log("Jump: " + counter);
                     }
                 }
                 else
                 {
-                    Debug.Log("Did not hit object");
+                    //Debug.Log("Did not hit object");
+                }
+
+                if (counter > 2)
+                {
+                    Debug.Log("Player has reached max jumps");
+                    counter = 0;
                 }
         
     }
